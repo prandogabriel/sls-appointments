c2c94b35110254438e73fd42995547dc78cc8f20
prandogabriel <gprando55@gmail.com>
2025-01-20 17:10:03 -0300

    fix: region definition to run locally

--- src/libs/dynamodb/client.ts
+++ src/libs/dynamodb/client.ts
@@ -2,9 +2,17 @@ import { DynamoDBClient, DynamoDBClientConfig } from "@aws-sdk/client-dynamodb";
 import { DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
 
 export const defaultLocalStackConfig: DynamoDBClientConfig = {
-  region: 'us-east-1',
-  endpoint: 'http://localhost:4566',
+  region: "us-east-1",
+  endpoint: "http://localhost:4566"
 };
 
-const client = new DynamoDBClient(process.env.ENVIRONMENT === "test" ? defaultLocalStackConfig : {});
+const client = new DynamoDBClient(
+  process.env.ENVIRONMENT === "test"
+    ? defaultLocalStackConfig
+    : {
+        region:
+          process.env.ENVIRONMENT === "production" ? "us-east-1" : "us-west-1"
+      }
+);
+
 export const docClient = DynamoDBDocumentClient.from(client);
